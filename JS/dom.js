b232d90148ea9eaa1945f31155053d72b7b9fcbf
carlaanahi12 <carla95bordon@hotmail.com>
2022-09-29 21:08:07 -0300

    se crearon eventos en el archivo dom.js y se crearon estilos css

--- JS/dom.js
+++ JS/dom.js
@@ -1,45 +1,65 @@
-const div = document.getElementsByClassName('sueldoIngresado');
-console.log(div);
+let sueldoBruto = '';
+let sueldoNeto = '';
+
+//LLAMO A LOS ELEMENTOS QUE VOY A UTILIZAR MEDIANTE DOM
+const input = document.getElementById('button');
+const botonCalcular = document.getElementById('CLICK');
+const resultado = document.getElementById("resultado");
 
 
-const sueldoBruto = prompt('ingrese sueldo bruto');
+//EVENTO INPUT
+input.addEventListener('input', () => {
+   sueldoBruto = (input.value);
+   console.log(sueldoBruto);
+});
 
 
 
-let obraSocial = 3;
-let jubilacion = 11;
-let leyObligatoria = 3;
+//EVENTO CLICK 
+botonCalcular.addEventListener('click', respuestaClick);
 
-function calcular(){
-    return sueldoBruto * obraSocial / 100;
+function respuestaClick (){
+    let obraSocial = 3;
+    let jubilacion = 11;
+    let leyObligatoria = 3;
+
+    function calcular(){
+        return sueldoBruto * obraSocial / 100;
+
+    }
+    let descObraSocial = calcular(sueldoBruto, obraSocial);
+    // console.log(descObraSocial);
+
+    function calcjubilacion(){
+        return sueldoBruto * jubilacion / 100;
+    }
+    let descJubilacion = calcjubilacion(sueldoBruto, jubilacion);
+    // console.log(descJubilacion);
+
+    function calcley (){
+        return sueldoBruto * leyObligatoria / 100;
+    }
+    let descLey = calcley(sueldoBruto, leyObligatoria);
+    // console.log(descLey);
+
+    sueldoNeto = sueldoBruto - descObraSocial - descJubilacion - descLey;
+
+console.log(sueldoNeto);
+
+actualizar();
 
 }
-let descObraSocial = calcular(sueldoBruto, obraSocial);
-console.log(descObraSocial);
 
 
-function calcjubilacion(){
-    return sueldoBruto * jubilacion / 100;
+// botonCalcular.onkeydown = () =>{
+//     respuestaClick();
+//     actualizar();
+
+// }
+
+
+const actualizar = () => {
+    resultado.innerHTML = `Tu sueldo neto es: $${sueldoNeto}`;
+
 }
-let descJubilacion = calcjubilacion(sueldoBruto, jubilacion);
-console.log(descJubilacion);
-
-function calcley (){
-    return sueldoBruto * leyObligatoria / 100;
-}
-let descLey = calcley(sueldoBruto, leyObligatoria);
-console.log(descLey);
-
-
-let sueldoNeto = sueldoBruto - descObraSocial - descJubilacion - descLey;
-
-
-let resultado = document.getElementById("resultado");
-resultado.innerHTML = `su sueldo neto es:  ${sueldoNeto}`;
-
-
-
-
-
-
 
